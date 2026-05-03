3728aa3e4f75164343f2bc9e4a9637e903dc92d9
Shain Wai Yan <shainwaiyan2002@gmail.com>
2026-05-03 10:26:00 +0630

    improve technical seo greatly and enhance blog page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@ import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sanity from "@sanity/astro";
+import sitemap from "@astrojs/sitemap";
 
 import vercel from "@astrojs/vercel";
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sanity({
+  site: "https://www.drnathanmarketing.com",
+  prefetch: {
+    prefetchAll: false,
+    defaultStrategy: "viewport",
+  },
+  integrations: [tailwind(), react(), sitemap(), sanity({
     projectId: "9w1ph2wu",
     dataset: "production",
     useCdn: true,
